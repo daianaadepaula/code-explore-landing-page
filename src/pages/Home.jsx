767c828294c7ad9d196f9c1a4f6554a08711e654
Daiana de Paula <daianaadepaula1@gmail.com>
2024-05-30 13:48:58 -0300

    feat: create home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Banner from "../shared/Banner";
+import homeImg from "./../assets/images/backgroundright.png";
+
+const Home = () => {
+  return (
+    <section className="md:px-12 p-4 max-w-screen-2xl mx-auto mt-28">
+
+      <Banner 
+				banner={homeImg}
+				heading={"Lorem ipsum dolor sit amet"}
+				subheading={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet quia adipisci molestias. Recusandae voluptas in magni, quae temporibus autem nulla sunt labore"}
+				btn1={"Vamos conversar?"} 
+				btn2={"Vamos conversar?"} 
+			/>
+
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/shared/Banner.jsx
@@ -0,0 +1,26 @@
+const Banner = ({ banner, heading, subheading, btn1, btn2 }) => {
+  return (
+    <div className="gradientBg rounded-xl rounded-br-[80px] md:p-9 px-5 py-10">
+      <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-10">
+
+        <div className="w-3/5 p-1 md:w-2/5">
+          <img src={banner} alt="" className="lg:h-[386px]" />
+        </div>
+
+        <div className="text-center md:text-left md:w-3/5">
+          <h2 className="md:text-7xl text-4xl font-bold text-white mb-6 leading-relaxed">
+            {heading}
+          </h2>
+          <p className="text-[#ebebeb] text-2xl mb-8">{subheading}</p>
+          <div className="space-x-5 space-y-4">
+            <button className="btn">{btn1}</button>
+            <button className="btn">{btn2}</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
